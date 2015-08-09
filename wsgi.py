c089096d9fa7b6474867e913bd0ac9f6bdcada4d
VitaliyVolkov <vitalyvolkov@inbox.ru>
2015-08-09 23:48:01 +0300

    update

--- wsgi.py
+++ wsgi.py
@@ -1,33 +1,51 @@
 from jinja2 import Template, Environment, FileSystemLoader
-from cgi import parse_qs, escape
-from wsgiref.util import request_uri
+import cgi
+import re
+import random
+import string
+import md5
 
 def application(environ, start_response):
 	#env = Environment(loader=FileSystemLoader('/home/user/myapp/templates/'))
 	#template = env.get_template('index.html')
-	html = open('/home/user/myapp/templates/base.html').read()
-	template = Template(html)
-	p1 = open('/home/user/myapp/templates/index/p1.html').read()
-	output = template.render(block1=p1, block2=u'BLOCK2', block3=u'BLOCK3')
+	#TEMPLATES BLOCK
+	main_page = Template(open('/home/user/myapp/templates/base.html').read())
+	p1 = open('/home/user/myapp/templates/test/p1.html').read()
+	article_id = int(open('/home/user/myapp/templates/test/test.html').read()) #temp for testing (id=2)
+	contacts_page = open('/home/user/myapp/templates/contacts.html').read()
+	page_404 = open('/home/user/myapp/templates/404.html').read()
 	
-	#parsing like from http://127.0.0.1/?age=10&hobbies=software&hobbies=tunning
+	#parsing uri like from http://127.0.0.1/?age=10&hobbies=software&hobbies=tunning
 	#d = parse_qs(environ['QUERY_STRING'])
 	#print (d)
 	
-	full_uri = request_uri(environ, include_query=1)
-	print(full_uri)
-	if full_uri == "http://localhost/" or full_uri == "http://127.0.0.1/":
-		output = "Main Page"
-	elif full_uri == "http://localhost/article/(?P<article_id>\d+)/$" or full_uri == "http://127.0.0.1/article/(?P<article_id>\d+)/$":
+	uri = environ['REQUEST_URI']
+	print(uri)
+
+	#Match (re.match()) objects are always True, and None if there's no match.
+	#URI BLOCK
+	if re.match(r'/article/%s/.*'%article_id, uri):
 		output = "Article page (ID important)"
-	elif full_uri == "http://localhost/reg/$" or full_uri == "http://127.0.0.1/reg/$":
+	elif re.match(r'/reg/.*', uri):
 		output = "Registration page"
-	elif full_uri == "http://localhost/partner/$" or full_uri == "http://127.0.0.1/partner/$":
+	elif re.match(r'/auth/.*', uri):
+		output = auth(environ, start_response)
+	elif re.match(r'/partner/.*', uri):
 		output = "Became a partner page"
-	elif full_uri == "http://localhost/contacts/$" or full_uri == "http://127.0.0.1/contacts/$":
-		output = "Contacts page"
+	elif re.match(r'/contacts/.*', uri):
+		output = main_page.render(myblock=contacts_page)
+	elif uri == "/":
+		output = main_page.render(myblock=p1)
 	else:
-		output = "404 page.html page TEST"
+		output = main_page.render(myblock=page_404)
 
 	start_response('200 OK', [('Content-Type', 'text/html; charset=utf-8'), ('Content-Length', str(len(output)))])
 	return [ output.encode("utf-8") ]
+
+def auth(environ, start_response):
+	key = "Blast"
+	session_id = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for x in range(32))
+	client_agent = environ['HTTP_USER_AGENT'] + key
+	ident_id = md5.new(client_agent).digest()
+	print(ident_id)
+	return "Session_id is " + session_id
