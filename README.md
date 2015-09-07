1dd6856894a7e56253652f440c62b7bbf6548424
VitaliyVolkov <vitalyvolkov@inbox.ru>
2015-09-07 10:47:32 +0300

    update

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-HELLO, RCWOOLF!
+Hello there! 
+
+This is my own project of the website blog, bulit on the chain:
+- Nginx
+- uwsgi
+- WSGI python app
+- Postgresql database.
+
+
+Project required following packages:
+
+BACKEND:
+- nginx/1.4.6
+- uwsgi/2.0.11.1
+- python/2.7.6
+- postgresql/9.3.9
+- psycopg2/2.6.1
+
+FRONTEND:
+- Jinja2 (Backend also required)
+- Bootstrap
+- jQuery
+- CKEditor
+
+
+List of required tables in Postgresql database:
+- CREATE TABLE users (id SERIAL PRIMARY KEY, name VARCHAR(64), login VARCHAR(64), password BYTEA);
+- CREATE TABLE articles (article_id SERIAL PRIMARY KEY, article_title VARCHAR(64), article_author VARCHAR(64), article_text VARCHAR(12000), article_date TIMESTAMP);
+- CREATE TABLE comments (comment_id SERIAL PRIMARY KEY, article_idc INTEGER, comment_author VARCHAR(64), comment_text VARCHAR(800), comment_date TIMESTAMP);
+- CREATE TABLE sessions (user_id INTEGER, session_id VARCHAR(32), ident_id BYTEA, date TIMESTAMP);
+- CREATE TABLE images (img_id SERIAL PRIMARY KEY);
+
+
+UPSTART FILE to Manage the App (on Ubuntu /etc/init/myapp.conf)
+
+description "uWSGI instance to serve myapp"
+
+start on runlevel [2345]
+stop on runlevel [!2345]
+
+setuid demo
+setgid www-data
+
+script
+    cd /home/demo/myapp
+    . myappenv/bin/activate
+    uwsgi --ini myapp.ini
+end script
+
+After this you can reload web-application with "sudo restart myapp"
--- static/js/ckeditor/config.js
+++ static/js/ckeditor/config.js
@@ -4,31 +4,18 @@
  */
 
 CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here.
-	// For complete reference see:
-	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
 
-	// The toolbar groups arrangement, optimized for two toolbar rows.
-	config.toolbarGroups = [
-		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
-		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
-		{ name: 'links' },
-		{ name: 'insert' },
-		{ name: 'forms' },
-		{ name: 'tools' },
-		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
-		{ name: 'others' },
-		'/',
-		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
-		{ name: 'styles' },
-		{ name: 'colors' },
-		{ name: 'about' }
+	config.toolbar = 'MyToolBar'
+
+	config.toolbar_MyToolBar = [
+		{ name: 'basicstyles', items : [ 'Bold','Italic','Underline','Strike','Subscript','Superscript','-','RemoveFormat' ] },
+		{ name: 'paragraph', items :[ 'NumberedList','BulletedList','-','Outdent','Indent','-','Blockquote','CreateDiv','-','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock','-','BidiLtr','BidiRtl' ] },
+		{ name: 'insert', items : [ 'Image','Flash','HorizontalRule','Smiley','SpecialChar','PageBreak' ] },
+		{ name: 'tools', items : [ 'Maximize' ] }
 	];
 
 	// Remove some buttons provided by the standard plugins, which are
 	// not needed in the Standard(s) toolbar.
-	config.removeButtons = 'Underline,Subscript,Superscript';
 
 	// Set the most common block elements.
 	config.format_tags = 'p;h1;h2;h3;pre';
@@ -43,4 +30,6 @@ CKEDITOR.editorConfig = function( config ) {
 	config.entities = false; 
 
 	config.basicEntities = false;
+
+	config.height = 400;
 };
